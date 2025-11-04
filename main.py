aec31036a374dffd75a186c3a764201276314468
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-11-04 23:17:25 +0330

    working on an aspect ratio maker

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def create_white_image(width_ratio, height_ratio):
     height = 128 * height_ratio
     width = 128 * width_ratio 
 
-    image_array = np.ones((height, width, 3), dtype=np.uint8) * 255 
-
+    image_array = np.zeros((height, width, 3), dtype=np.uint8) * 255 
+    
     return image_array
 
 image_array = create_white_image(1, 1)
