a8433d4cdfa6e888e9ec010947a4a1539dad393d
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-11-04 23:03:56 +0330

    a different thing has been made

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def create_white_image(width_ratio, height_ratio):
     height = 1024 * height_ratio
     width = 1024 * width_ratio 
 
-    image_array = np.ones((width, height, 3), dtype=np.uint8) * 255 
+    image_array = np.ones((height, width, 3), dtype=np.uint8) * 255 
 
     return image_array
 
