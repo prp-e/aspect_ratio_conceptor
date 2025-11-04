cd142f231262a6d10524e3b8cda528cddb8d0266
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-11-04 23:02:02 +0330

    now it's only one function

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import cv2
 import numpy as np 
 
-height = 1536
-width = 1024
-
-image_array = np.ones((width, height, 3), dtype=np.uint8) * 255 
-
-cv2.imwrite("final_image.jpg", image_array)
-
 def create_white_image(width_ratio, height_ratio):
-    pass
+    height = 1024 * height_ratio
+    width = 1024 * width_ratio 
+
+    image_array = np.ones((width, height, 3), dtype=np.uint8) * 255 
+
+    return image_array
